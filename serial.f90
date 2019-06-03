06e794096e5da6ea2aa7cea259033919c88465c7
jfactor <jfactor@ucsc.edu>
2019-06-02 19:41:53 -0700

    finished serial part

--- serial.f90
+++ serial.f90
@@ -2,16 +2,18 @@ program serial
 
   implicit none
   integer, dimension(:,:), allocatable :: A
+  integer, dimension(:,:), allocatable :: newA
   integer :: n = 4  ! A = n x n matrix 
-  integer i, j 
+  integer i, j , x , y, counter, xpos, ypos
   integer dateTime(8);
 
 
   call date_and_time(values = dateTime)
-  allocate(A(n,n))
+  allocate(A(0:n-1,0:n-1))
+  allocate(newA(0:n-1, 0:n-1))
   call srand(dateTime(8))
-  do j = 1 , n
-    do i = 1, n
+  do j = 0 , n-1
+    do i = 0, n-1
       if( rand() * 6 <= 3) then 
         A(i,j) = 1
       else 
@@ -20,9 +22,41 @@ program serial
     end do 
   end do
   
-  do i = 1, n 
+  print *, 'A:'
+  do i = 0, n-1 
     print *, A(i,:)
+  end do
+ 
+  do x = 0 , n-1
+    do y = 0 , n-1
+      counter = 0 
+      do i = -1, 1
+        do j = -1, 1
+          xpos = mod(x + i + n, n) 
+          ypos = mod(y + i + n, n)
+          if(.not. (j==0 .and. i==0) .and. A(xpos,ypos) == 1) then 
+            counter = counter + 1
+          end if 
+        end do 
+      end do
+      if(counter == 3) then 
+        newA(x,y) = 1
+      else if(counter == 2) then 
+        newA(x,y) = A(x,y)
+      else 
+        newA(x,y) = 0
+      end if 
+
+    end do 
   end do 
+
+  
+  print *, 'newA:'
+  do i = 0, n-1 
+    print *, newA(i,:)
+  end do
+ 
   deallocate(A) 
+  deallocate(newA)
 
 end program serial
